816b113a83729842c43177cb91398c592e0ab304
Akatosh <romaspost4work@gmail.com>
2019-09-12 21:07:56 +0300

    'parseMsg

--- servers/http/app.js
+++ servers/http/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const MailParser = require("mailparser-mit").MailParser;
+const mailparser = new MailParser();
 
 app.get('/', (req,res)=>{
     let msg = {};
     fs.readdir(path.join(__dirname, '/../', 'smtp', '/msg'), (err, files)=>{
             if(err) msg = [{'err':'No directory'}];
             else {
+
                 files.forEach((fileName)=>{
                     let tmp = JSON.parse(fs.readFileSync(path.join(__dirname, '/../', 'smtp', '/msg', fileName), 'utf8'));
                     msg[fileName.substring(0, fileName.indexOf('.json'))] = tmp;
--- servers/smtp/app.js
+++ servers/smtp/app.js
@@ -1,16 +1,27 @@
 const net = require('net');
 const fs = require('fs');
 const path = require('path');
-let expectedCommand = require('./commands.js');
+const crypto = require("crypto");
+const MailParser = require("mailparser-mit").MailParser;
+const mailparser = new MailParser();
+
+function parseMsg(body){
+    mailparser.write(body);
+    return new Promise(resolve => {
+        mailparser.on("end", function(mail_object){
+            resolve(mail_object);
+        });
+        mailparser.end();
+    });
+}
 
 const server = net.createServer((socket) => {
+let expectedCommand = require('./commands.js');
     socket.setEncoding('utf8');
     socket.setTimeout(15000);
     let dataString = '';
     let msg = {};
-    console.log('connected');
     socket.on('data', (str)=>{
-        console.log(str);
         for(let data in str){
             if(str[data].charCodeAt(0) == 13){
                 if(dataString.toUpperCase().split(' ')[0] == 'RSET'){
@@ -19,32 +30,40 @@ const server = net.createServer((socket) => {
                     socket.end('221 | closing connection | bb =B\u000D\u000A');
                 }else {
                     let executed = expectedCommand(dataString);
-                    console.log(executed.response)
                     if(executed.status == 'close'){
                         socket.end(executed.response);
                     }else if(!executed.status){
                         socket.write(executed.response);
                     }else {
                         if(executed['ended']){
-                            fs.readFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), 'utf8', (err, data)=>{
-                                if(err){
-                                    fs.writeFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), '['+JSON.stringify(msg)+']', (err)=>{
-                                        if(err) socket.write('500 | Fs system error\u000D\u000A');
-                                        msg = {};
-                                        socket.write('250 | Done\u000D\u000A');
-                                    });
-                                }else {
-                                    let tmp = JSON.parse(data);
-                                    tmp.push(msg);
-                                    tmp = JSON.stringify(tmp);
-                                    fs.writeFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), tmp, (err)=>{
-                                        if(err) socket.write('500 | Fs system error\u000D\u000A');
-                                        msg = {};
-                                        socket.write('250 | Done\u000D\u000A');
-                                    });
-                                }
+                            parseMsg(msg.data).then(body=>{
+                                msg.data = body;
+                                msg.id = crypto.randomBytes(16).toString("hex");
+                                fs.readFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), 'utf8', (err, data)=>{
+                                    if(err){
+                                        fs.writeFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), '['+JSON.stringify(msg)+']', (err)=>{
+                                            if(err) socket.write('500 | Fs system error\u000D\u000A');
+                                            else {
+                                                addMsg(msg.receiver, msg.sender, msg.id);
+                                            }
+                                            msg = {};
+                                            socket.write('250 | Done\u000D\u000A');
+                                        });
+                                    }else {
+                                        let tmp = JSON.parse(data);
+                                        tmp.push(msg);
+                                        tmp = JSON.stringify(tmp);
+                                        fs.writeFile(path.join(__dirname, '/msg/', msg.receiver.split('@')[0] + '.json'), tmp, (err)=>{
+                                            if(err) socket.write('500 | Fs system error\u000D\u000A');
+                                            else {
+                                                addMsg(msg.receiver, msg.sender, msg.id);
+                                            }
+                                            msg = {};
+                                            socket.write('250 | Done\u000D\u000A');
+                                        });
+                                    }
+                                });
                             });
-
                         }else {
                             for(let a in executed.data){
                                 if(msg[a]) msg[a] += executed.data[a];
@@ -69,6 +88,22 @@ const server = net.createServer((socket) => {
         msg = {};
     }
 });
+
+function addMsg(mailBox, from, id, subject = '') {
+    fs.readFile(path.join(__dirname, 'msgData.json'), 'utf8', (err, data)=>{
+        if(!err){
+            var tmp = JSON.parse(data);
+            tmp.push({
+                'id':id,
+                'receiver':mailBox,
+                'from':from,
+                'subject':subject,
+                'read':false
+            });
+            fs.writeFile(path.join(__dirname, 'msgData.json'), JSON.stringify(tmp), (err)=>{});
+        }
+    });
+}
 
 server.on('connection', (socket)=>{
     socket.write('220 | onyame.ml | ESMTP\u000D\u000A');
